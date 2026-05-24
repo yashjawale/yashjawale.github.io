bd417fcba6cd499254765eb7a53b1f6504c989d1
yashjawale <yash25.j@gmail.com>
2026-05-24 09:26:59 +0530

    fix: correct collection name from 'blog' to 'blogs' in RSS feed generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-	const blog = await getCollection('blog')
+	const blog = await getCollection('blogs')
 	return rss({
 		// `<title>` field in output xml
 		title: 'Yash Jawale',
